3e0664a2a0f6b1462b70afb965827c2f5710f253
Marcin <marcin.smola7@gmail.com>
2018-01-17 19:38:17 +0100

    adapter design pattern

---
+++ src/com/smola/after/LineAdapter.java
@@ -0,0 +1,14 @@
+package com.smola.after;
+
+public class LineAdapter implements Shape {
+    private Line adaptee;
+
+    public LineAdapter(Line adaptee) {
+        this.adaptee = adaptee;
+    }
+
+    @Override
+    public void draw(int x, int y, int z, int j) {
+        adaptee.draw(x,y,z,j);
+    }
+}
---
+++ src/com/smola/after/Shape.java
@@ -0,0 +1,5 @@
+package com.smola.after;
+
+public interface Shape {
+    void draw(int x, int y, int z, int j);
+}
